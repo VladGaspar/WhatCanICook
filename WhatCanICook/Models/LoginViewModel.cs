db484195d63c387acbb0ade9252906bcc15e84db
VLADPC\Vlad <jimbo18yt@gmail.com>
2023-05-08 22:27:27 +0300

    Functional Login

--- WhatCanICook/Models/LoginViewModel.cs
+++ WhatCanICook/Models/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace WhatCanICook.Models
     public class LoginViewModel
     {
         public string Email { get; set; }
+
         public string Password { get; set; }
     }
 }
